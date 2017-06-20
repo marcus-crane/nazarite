1daea10f8f319625b86a1649cdb872e9e8513f12
Marcus <marcus@thingsima.de>
2017-06-20 14:57:58 +1200

    Added user agent check

--- js/responsiveness.js
+++ js/responsiveness.js
@@ -6,32 +6,42 @@ var story = document.getElementById('story')
 // 660px width
 
 var width = document.querySelector('body').offsetWidth
-console.log(width)
 
-if (width > 0 && width <= 665) {
-  trees.dataset.rellaxSpeed = -2
-  rocks.dataset.rellaxSpeed = 10
-  scroll.dataset.rellaxSpeed = -6
-  story.dataset.rellaxSpeed = -6
+if (navigator.userAgent.includes('Chrome')) {
+  if (width > 0 && width <= 665) {
+    trees.dataset.rellaxSpeed = -2
+    rocks.dataset.rellaxSpeed = 10
+    scroll.dataset.rellaxSpeed = -6
+    story.dataset.rellaxSpeed = -6
+  }
+
+  if (width >= 666 && width <= 800) {
+    trees.dataset.rellaxSpeed = -2
+    rocks.dataset.rellaxSpeed = 10
+    scroll.dataset.rellaxSpeed = -7
+    story.dataset.rellaxSpeed = -7
+  }
+
+  if (width >= 801 && width <= 1000) {
+    trees.dataset.rellaxSpeed = -2
+    rocks.dataset.rellaxSpeed = 10
+    scroll.dataset.rellaxSpeed = -8
+    story.dataset.rellaxSpeed = -8
+  }
+
+  if (width > 1000) {
+    trees.dataset.rellaxSpeed = -2
+    rocks.dataset.rellaxSpeed = 10
+    scroll.dataset.rellaxSpeed = -8
+    story.dataset.rellaxSpeed = -10
+  }
 }
 
-if (width >= 666 && width <= 800) {
-  trees.dataset.rellaxSpeed = -2
-  rocks.dataset.rellaxSpeed = 10
-  scroll.dataset.rellaxSpeed = -7
-  story.dataset.rellaxSpeed = -7
-}
-
-if (width >= 801 && width <= 1000) {
-  trees.dataset.rellaxSpeed = -2
-  rocks.dataset.rellaxSpeed = 10
-  scroll.dataset.rellaxSpeed = -8
-  story.dataset.rellaxSpeed = -8
-}
-
-if (width > 1000) {
-  trees.dataset.rellaxSpeed = -2
-  rocks.dataset.rellaxSpeed = 10
-  scroll.dataset.rellaxSpeed = -8
-  story.dataset.rellaxSpeed = -10
+if (navigator.userAgent.includes('Firefox')) {
+  if (width > 1000) {
+    trees.dataset.rellaxSpeed = -2
+    rocks.dataset.rellaxSpeed = 10
+    scroll.dataset.rellaxSpeed = -8
+    story.dataset.rellaxSpeed = -10
+  }
 }
