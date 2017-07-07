cfe42b3a1f70349ab92340d5cc887fa30816afd4
Marcus <marcus@thingsima.de>
2017-07-07 19:45:48 +1200

    Fiddled with media queries again

--- js/responsiveness.js
+++ js/responsiveness.js
@@ -8,30 +8,37 @@ var story = document.getElementById('story')
 
 var width = document.querySelector('body').offsetWidth
 
-if (width > 0 && width <= 665) {
+if (width > 0 && width <= 399) {
+  trees.dataset.rellaxSpeed = -2
+  rocks.dataset.rellaxSpeed = 10
+  scroll.dataset.rellaxSpeed = -5
+  story.dataset.rellaxSpeed = -1
+}
+
+if (width > 400 && width <= 665) {
   trees.dataset.rellaxSpeed = -2
   rocks.dataset.rellaxSpeed = 10
   scroll.dataset.rellaxSpeed = -6
-  story.dataset.rellaxSpeed = -3
+  story.dataset.rellaxSpeed = -1.5
 }
 
 if (width >= 666 && width <= 800) {
   trees.dataset.rellaxSpeed = -2
   rocks.dataset.rellaxSpeed = 10
   scroll.dataset.rellaxSpeed = -7
-  story.dataset.rellaxSpeed = -3
+  story.dataset.rellaxSpeed = -2
 }
 
 if (width >= 801 && width <= 1000) {
   trees.dataset.rellaxSpeed = -2
   rocks.dataset.rellaxSpeed = 10
   scroll.dataset.rellaxSpeed = -8
-  story.dataset.rellaxSpeed = -4
+  story.dataset.rellaxSpeed = -2
 }
 
 if (width > 1000) {
   trees.dataset.rellaxSpeed = -2
   rocks.dataset.rellaxSpeed = 10
   scroll.dataset.rellaxSpeed = -8
-  story.dataset.rellaxSpeed = -6
+  story.dataset.rellaxSpeed = -5
 }
