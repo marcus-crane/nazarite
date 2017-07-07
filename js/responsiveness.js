fe93fb21014a0364f381a3374cb33d4c4e3e054a
Marcus <marcus@thingsima.de>
2017-07-07 18:23:05 +1200

    Removed user agent locks and lower parallax speed a little

--- js/responsiveness.js
+++ js/responsiveness.js
@@ -8,41 +8,30 @@ var story = document.getElementById('story')
 
 var width = document.querySelector('body').offsetWidth
 
-if (navigator.userAgent.includes('Chrome')) {
-  if (width > 0 && width <= 665) {
-    trees.dataset.rellaxSpeed = -2
-    rocks.dataset.rellaxSpeed = 10
-    scroll.dataset.rellaxSpeed = -6
-    story.dataset.rellaxSpeed = -3
-  }
-
-  if (width >= 666 && width <= 800) {
-    trees.dataset.rellaxSpeed = -2
-    rocks.dataset.rellaxSpeed = 10
-    scroll.dataset.rellaxSpeed = -7
-    story.dataset.rellaxSpeed = -3
-  }
-
-  if (width >= 801 && width <= 1000) {
-    trees.dataset.rellaxSpeed = -2
-    rocks.dataset.rellaxSpeed = 10
-    scroll.dataset.rellaxSpeed = -8
-    story.dataset.rellaxSpeed = -4
-  }
-
-  if (width > 1000) {
-    trees.dataset.rellaxSpeed = -2
-    rocks.dataset.rellaxSpeed = 10
-    scroll.dataset.rellaxSpeed = -8
-    story.dataset.rellaxSpeed = -7
-  }
+if (width > 0 && width <= 665) {
+  trees.dataset.rellaxSpeed = -2
+  rocks.dataset.rellaxSpeed = 10
+  scroll.dataset.rellaxSpeed = -6
+  story.dataset.rellaxSpeed = -3
 }
 
-if (navigator.userAgent.includes('Firefox')) {
-  if (width > 1000) {
-    trees.dataset.rellaxSpeed = -2
-    rocks.dataset.rellaxSpeed = 10
-    scroll.dataset.rellaxSpeed = -8
-    story.dataset.rellaxSpeed = -4
-  }
+if (width >= 666 && width <= 800) {
+  trees.dataset.rellaxSpeed = -2
+  rocks.dataset.rellaxSpeed = 10
+  scroll.dataset.rellaxSpeed = -7
+  story.dataset.rellaxSpeed = -3
+}
+
+if (width >= 801 && width <= 1000) {
+  trees.dataset.rellaxSpeed = -2
+  rocks.dataset.rellaxSpeed = 10
+  scroll.dataset.rellaxSpeed = -8
+  story.dataset.rellaxSpeed = -4
+}
+
+if (width > 1000) {
+  trees.dataset.rellaxSpeed = -2
+  rocks.dataset.rellaxSpeed = 10
+  scroll.dataset.rellaxSpeed = -8
+  story.dataset.rellaxSpeed = -6
 }
